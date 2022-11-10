4c49700e4e22f7954b632407a36ef520598c3fb4
EC2 Default User <ec2-user@ip-172-31-0-210.ap-northeast-1.compute.internal>
2022-11-10 14:51:37 +0000

    [finish]nagano_cake.11/10

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,38 +1,38 @@
 class Admin::ItemsController < ApplicationController
-  
+
   def index
     @items = Item.all
   end
-  
+
   def new
+    @item = Item.new
     @genres = Genre.all
-    @genre = Genre.new
   end
-  
+
   def create
-    @genres = Genre.all
-    @genre = Genre.new
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item.id)
   end
-  
+
   def show
-    @genres = Genre.all
-    @genre = Genre.new
+   
   end
-  
+
   def edit
-    @genres = Genre.all
-    @genre = Genre.new
+    
   end
-  
+
   def update
-    @genres = Genre.all
-    @genre = Genre.new
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_item_path(@item.id)
   end
-  
+
 private
 
   def item_params
-    params.require(:item).permit(:name,:introduction,:price)
+    params.require(:item).permit(:item_image,:name,:introduction,:price,:genre_id,:is_active)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,16 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_items, dependent: :destroy
   belongs_to :genre
-  has_one_attached :image
-  
+  has_one_attached :item_image
 end
+  
+  
+def get_item_image(width, height)
+  unless item_image.attached?
+    file_path = Rails.root.join('app/assets/images/no_item_image.jpg')
+    item_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  end
+  item_image.variant(resize_to_limit: [width, height]).processed
+end
+  
+
