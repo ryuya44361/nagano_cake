3c26d74525df2653d74d944ae397eda690aa0a26
EC2 Default User <ec2-user@ip-172-31-0-210.ap-northeast-1.compute.internal>
2022-11-11 13:25:38 +0000

    [finish]nagano_cake.11/11

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ItemsController < ApplicationController
 
   def index
-    @items = Item.all
+    @items = Item.page(params[:page])
   end
 
   def new
@@ -16,7 +16,7 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
-   
+   @item = Item.find(params[:id])
   end
 
   def edit
