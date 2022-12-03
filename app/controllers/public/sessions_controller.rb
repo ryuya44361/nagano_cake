30ff85be3941641c92f9adf0eb1d80d4c695cee0
EC2 Default User <ec2-user@ip-172-31-0-210.ap-northeast-1.compute.internal>
2022-12-03 17:03:24 +0000

    [finish]nagano_cake.12/03

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -3,8 +3,16 @@
 class Public::SessionsController < Devise::SessionsController
   before_action :customer_state, only: [:create]
 
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
   protected
-  
+
   def customer_state
      @customer = Customer.find_by(email: params[:customer][:email])
      return if !@customer
